0e0db56586ab1405615263fc42ce0ca25ea5b23c
SOMRAJ <75350513+iamsomrajg@users.noreply.github.com>
2025-07-10 11:37:49 -0500

    Update main.py

--- Day-02-variables/main.py
+++ Day-02-variables/main.py
@@ -3,3 +3,8 @@ var = 10
 
 # f prefix is used before the opening quote while calling any variables names to create a difference between the variable name inside curly braces and string 
 print(f'var = {var}')
+
+"""
+Output:
+10
+"""
