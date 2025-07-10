c0018921966896e72f6547c8130c8a251226c9f1
SOMRAJ <75350513+iamsomrajg@users.noreply.github.com>
2025-07-10 11:36:46 -0500

    Create main.py

---
+++ Day-02-variables/main.py
@@ -0,0 +1,5 @@
+# here variable type is int which python can detect automatically
+var = 10
+
+# f prefix is used before the opening quote while calling any variables names to create a difference between the variable name inside curly braces and string 
+print(f'var = {var}')
