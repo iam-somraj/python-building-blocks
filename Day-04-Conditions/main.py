e0108eeb20bb937fbc5ec90e37a4ca384dad1b9b
SOMRAJ <75350513+iamsomrajg@users.noreply.github.com>
2025-07-11 06:32:45 -0500

    Update main.py

--- Day-04-Conditions/main.py
+++ Day-04-Conditions/main.py
@@ -5,3 +5,20 @@ if age > 18:
     status = "Adult"
 
 print(f"You're a {status}") # You're a Adult
+
+#----------------------------------------------------------
+
+#If - Else Statement
+wind = int(input()) #20
+status = "undefined"
+
+if wind < 8:
+    status = "Calm"
+elif wind >= 8 and wind <= 63:
+    status = "Breeze"
+else:
+    status = "Storm"          
+
+print(f"status = {status}") #Breeze
+
+#-----------------------------------------------------------
