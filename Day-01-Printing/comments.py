03187da2aab1974958c1547d37694d3f88fbd345
SOMRAJ <75350513+iamsomrajg@users.noreply.github.com>
2025-07-11 05:41:41 -0500

    Update comments.py

--- Day-01-Printing/comments.py
+++ Day-01-Printing/comments.py
@@ -1,9 +1,10 @@
 Single-line comments using the '#' symbol:
-# This is a comment
+# This is a example comments
+
 
 Multi-line comments using triple quotes ("""):
 """
 Hi
 Hello World!
-This is a multi-line comment.
+This is a multi-line example comment.
 """
