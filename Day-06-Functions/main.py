8a78ef782350f001dbf3c6b50dcc3f3f946cd236
SOMRAJ <75350513+iam-somraj@users.noreply.github.com>
2025-07-18 13:34:58 -0500

    Update main.py

--- Day-06-Functions/main.py
+++ Day-06-Functions/main.py
@@ -1,1 +1,20 @@
+no_of = int(input())
 
+def cal_sol():
+    sum = 0
+    for i in range(1, 10001):
+        sum += i
+    print(sum)
+
+for i in range(no_of):
+    cal_sol()
+
+#Input:
+5
+
+#Output:
+50005000
+50005000
+50005000
+50005000
+50005000
