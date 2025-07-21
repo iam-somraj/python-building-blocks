b53f35f3d199a5bfd7ae87609133af7734fe33c3
SOMRAJ <iamsomrajghosh@gmail.com>
2025-07-21 11:37:18 -0500

    Update main.py

--- Day-06-Functions/main.py
+++ Day-06-Functions/main.py
@@ -42,5 +42,13 @@ for i in range(no_of):
 #--------------------------------------------------------
 
 #Arguments:
+def is_even(number):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
 
-
+for i in range(15, 34):
+    is_even(i)
+for i in range(153, 219):
+    is_even(i)
