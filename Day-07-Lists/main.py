b10bf5def7dda81ead71c2adea0a99ee62400151
SOMRAJ <75350513+iam-somraj@users.noreply.github.com>
2025-08-12 10:32:01 -0500

    Update main.py

--- Day-07-Lists/main.py
+++ Day-07-Lists/main.py
@@ -1,1 +1,8 @@
-print("Lists")
+#Declaration of lists:
+stationery = ["pen", "pencil", "copy", "books"]
+
+#Accessing of Lists:
+stationery = ["pen", "pencil", "copy", "books"]
+imp_item = stationery[3]
+
+print(imp_item)
