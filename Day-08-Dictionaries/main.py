a9fa87527ff75ab0bda39bb8e3ced26584bb9759
SOMRAJ <75350513+iam-somraj@users.noreply.github.com>
2025-08-16 00:08:16 +0530

    Update main.py

--- Day-08-Dictionaries/main.py
+++ Day-08-Dictionaries/main.py
@@ -10,3 +10,17 @@ def create_student_dict(name, age, major):
 #Output:
 {'name': 'Alice', 'age': 20, 'major': 'Computer Science'}
 
+#Accessing the values
+country_capitals = {
+ "USA": "Washington, D.C.",
+ "France": "Paris",
+ "Japan": "Tokyo"
+}
+
+# Accessing values using keys
+print(country_capitals["USA"])
+print(country_capitals["France"])
+
+#Output:
+Washington, D.C.
+Paris
