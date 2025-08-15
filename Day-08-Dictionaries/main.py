d5f5195e31355e305b0a5c3da2aaa267de64a238
SOMRAJ <75350513+iam-somraj@users.noreply.github.com>
2025-08-16 00:06:07 +0530

    Update main.py

--- Day-08-Dictionaries/main.py
+++ Day-08-Dictionaries/main.py
@@ -1,1 +1,12 @@
+def create_student_dict(name, age, major):
+    student_dict = {}
+
+    student_dict["name"] = name
+    student_dict["age"] = age
+    student_dict["major"] = major
+
+    return student_dict
+
+#Output:
+{'name': 'Alice', 'age': 20, 'major': 'Computer Science'}
 
